5557f57e9cf2ff65bee1604c1b386f8aa85eac8f
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2023-11-20 17:33:41 +0900

    開発用に条件分岐を回避

--- src/glossary.py
+++ src/glossary.py
@@ -9,12 +9,18 @@ def main():
     # NIAD用語集の用語URL一覧を取得する
     niad_term_urls, niad_updated = niad_glossary.extract_glossary_url_list(config)
     niad_terms = ["JA\tEN\tDETAILS_JA\tDETAILS_EN\tURL"]
+    """
     if niad_updated:
         # NIAD用語URL一覧が更新された場合、用語集の詳細をあらためて取得する
         for term_url, _ in niad_term_urls:
             niad_terms.append("\t".join(niad_glossary.get_glossary_details(term_url)))
     else:
         print("NIAD用語集の更新はありませんでした。")
+    """
+    ### 以下、開発用 ###
+    for term_url, _ in niad_term_urls:
+        niad_terms.append("\t".join(niad_glossary.get_glossary_details(term_url)))
+    ### 以上、開発用 ###
     if not os.path.exists(config["output"]["dir"]):
         os.makedirs(config["output"]["dir"])
     with open(
