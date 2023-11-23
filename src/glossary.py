c038ca92866d57552748711ef6d4520c6ff26b8d
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-11-24 05:28:41 +0900

    NIAD用語集と自社用語集を結合したものを、DeepL用語集として整形して出力する処理追加

--- src/glossary.py
+++ src/glossary.py
@@ -12,8 +12,17 @@ def main():
     # 自社用語集の用語URL一覧を取得する
     my_glossary_list = gsheets_glossary.get_my_glossary(config)
     # 用語集一覧を結合する
-    # config.gsheets_glossary.is_priorityの値によって、優先する用語集を決定して結合する
-    pass
+    deepl_glossary_list = utils.merge_glossary_lists(
+        niad_glossary_list, my_glossary_list, config["gsheets_glossary"]["is_priority"]
+    )
+    # 結合した用語集一覧を出力する
+    with open(
+        os.path.join(config["output"]["dir"], config["output"]["deepl_glossary"]),
+        mode="w",
+        encoding="utf-8",
+    ) as f:
+        glossary = ["\t".join(row) for row in deepl_glossary_list]
+        f.write("\n".join(glossary))
 
 
 if __name__ == "__main__":
