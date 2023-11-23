dd4a5772c8654858db7b479941eb85bf2781b976
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-11-24 04:23:38 +0900

    自社用語集をGoogleスプレッドシートから取得する処理を追加

--- src/glossary.py
+++ src/glossary.py
@@ -10,7 +10,7 @@ def main():
     # NIAD用語集一覧を取得する
     niad_glossary_list = niad_glossary.get_niad_glossary(config)
     # 自社用語集の用語URL一覧を取得する
-    # my_glossary_list = gsheets_glossary.get_gsheets_glossary(config)
+    my_glossary_list = gsheets_glossary.get_my_glossary(config)
     # 用語集一覧を結合する
     # config.gsheets_glossary.is_priorityの値によって、優先する用語集を決定して結合する
     pass
